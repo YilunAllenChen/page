0722be46f5d1964222530279808d50fedc923caf
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-10-19 21:36:06 -0500

    make homepage nicer

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -32,16 +32,16 @@ impl Component for Contact {
                     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 bg-opacity-50">
                         <div class="text-center">
                             <h1 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-4xl">{"Let's get in touch!"}</h1>
-                            <div class="mt-10 flex items-center justify-center gap-x-6 mb-20">
+                            <div class="mt-10 flex items-center justify-center gap-x-6 mb-10">
                                 <a class="rounded-md disabled bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     href="mailto:allenchenyilun1999@gmail.com"
-                                >{"📧Email Me📧"}</a>
-                                <button
-                                    onclick={ctx.props().on_clicked.reform(|_| Page::Home)}
-                                    class="rounded-md disabled bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                    {"Home"}
-                                </button>
+                                >{"Email Me @ allenchenyilun1999@gmail.com"}</a>
                             </div>
+                            <button
+                                onclick={ctx.props().on_clicked.reform(|_| Page::Home)}
+                                class="rounded-md disabled bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                {"Home"}
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -26,13 +26,13 @@ impl Component for Home {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div class="bg-black h-full bg-cover bg-center" style="background-image: url('./assets/robot_and_seagull.png')">
+            <div class="bg-black h-screen bg-cover bg-center" style="background-image: url('./assets/robot_and_seagull.png')">
                 <div class="relative isolate px-6 pt-14 lg:px-8">
                     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 bg-opacity-50">
                         <div class="text-center">
-                            <h1 class="text-4xl font-bold tracking-wider text-gray-100 sm:text-6xl font-mono">{"Allen Chen"}</h1>
-                            <p class="mt-6 text-lg leading-8 text-gray-300">{"I Build Stuff!"}</p>
-                            <div class="mt-10 flex flex-wrap items-center justify-center gap-x-6 mb-20">
+                            <h1 class="text-2xl md:text-4xl font-bold tracking-wider text-gray-100 sm:text-6xl font-mono">{r#"Yilun "Allen" Chen"#}</h1>
+                            <p class="mt-6 text-sm md:text-lg leading-8 text-gray-200">{"I Build Stuff!"}</p>
+                            <div class="mt-10 flex flex-wrap items-center justify-center gap-x-6 mb-10">
                                 <button
                                     onclick={ctx.props().on_clicked.reform(|_| Page::Projects)}
                                     class="rounded-md w-full md:w-1/4 bg-indigo-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
@@ -44,6 +44,7 @@ impl Component for Home {
                                     {"Contact Me"}
                                 </button>
                             </div>
+                            <p class="mt-2 text-xs md:text-base text-bold text-gray-300">{"Built with Rust/Yew, Tailwind CSS and ❤️"}</p>
                         </div>
                     </div>
                 </div>
