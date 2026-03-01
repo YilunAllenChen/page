c5aeb1cb7fd9604bfe9a8aed871c07be9e3d0412
YilunAllenChen <allenchenyilun1999@gmail.com>
2026-03-01 08:47:51 -0600

    small update

--- src/pages/projects.rs
+++ src/pages/projects.rs
@@ -34,10 +34,11 @@ pub fn my_component() -> Html {
 
     html! {
     <div class=r#"
-            bg-black mx-auto mt-10 grid px-8 grid-cols-1 gap-x-8 gap-y-16 pt-10 
-            md:mx-16 md:gap-x-32 md:grid-cols-2 
-            lg:gap-x-32 lg:px-32
-            xl:px-4
+            bg-black mx-auto mt-10 grid grid-cols-1 gap-x-8 gap-y-16 px-8 pt-10
+            md:grid-cols-2 md:px-12
+            lg:px-20
+            xl:grid-cols-3 xl:px-24
+            2xl:max-w-[1900px] 2xl:px-40
     "#
          onclick={onclick}>
         {articles}
